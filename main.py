0cbc80d6954f747f341cab67557b5c16f44ea677
Mely0517 <perezjones.melitza@gmail.com>
2025-07-26 19:37:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import discord
-from discord.ext import commands
-import os
 from temu import setup_bot
 
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-setup_bot(bot)
+bot = setup_bot()
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(bot.start(os.getenv("DISCORD_TOKEN")))
+
+    async def main():
+        await bot.start(os.getenv("DISCORD_TOKEN"))
+
+    asyncio.run(main())
