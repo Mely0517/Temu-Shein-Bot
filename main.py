0a00b0f94ddd6018e757879823c9986d64743d2c
Mely0517 <perezjones.melitza@gmail.com>
2025-07-26 19:54:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from temu import setup_bot
+import os
 
 bot = setup_bot()
-
-if __name__ == "__main__":
-    import asyncio
-
-    async def main():
-        await bot.start(os.getenv("DISCORD_TOKEN"))
-
-    asyncio.run(main())
+bot.run(os.getenv("DISCORD_TOKEN"))
