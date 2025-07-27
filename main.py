e1efe094b3e232a4956bddcd2bd143764f5ebc87
Mely0517 <perezjones.melitza@gmail.com>
2025-07-26 20:56:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
+from temu import bot, setup_bot
 import os
-import discord
-from discord.ext import commands
-from temu import setup_bot
-from dotenv import load_dotenv
 
-load_dotenv()
-
-intents = discord.Intents.default()
-intents.message_content = True  # Needed to read messages
-intents.guilds = True
-intents.members = True
-
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-setup_bot(bot)
-
-bot.run(os.getenv("DISCORD_TOKEN"))
+if __name__ == "__main__":
+    setup_bot()
+    bot.run(os.getenv("DISCORD_TOKEN"))
