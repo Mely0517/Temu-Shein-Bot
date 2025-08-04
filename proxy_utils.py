78dd0b71f1ddbf1c292adf00d0a7b451285c9026
Mely0517 <perezjones.melitza@gmail.com>
2025-08-04 00:22:05 -0600

    Update proxy_utils.py

--- proxy_utils.py
+++ proxy_utils.py
@@ -5,10 +5,11 @@ PROXIES = [
         "ip": "geo.iproyal.com",
         "port": "12321",
         "username": "melitza0517",
-        "password": "PpCMxtvpv1VpA"
+        "password": "PpCMxtvpv1VpA",
+        "country": "US"
     },
+    # Add more here
 ]
 
 def get_random_proxy():
     return random.choice(PROXIES)
-    
