50658f001913ef325da83ec823251800d7696fae
Mely0517 <perezjones.melitza@gmail.com>
2025-08-03 20:37:05 -0600

    Update proxy_utils.py

--- proxy_utils.py
+++ proxy_utils.py
@@ -1,19 +1,20 @@
 import itertools
 
-# ✅ Working HTTP proxies for testing (you can replace with premium ones)
-# Format must be either http://ip:port or http://user:pass@ip:port
+# ✅ Your IPRoyal authenticated proxies in the correct format for pyppeteer
 proxies = [
-    "http://103.172.70.45:7070",
-    "http://47.88.11.3:8080",
-    "http://138.2.86.245:3128",
-    "http://207.2.120.117:80",
-    "http://193.123.103.34:8080",
-    # Add more here
+    "http://PpCMxtvpv1VpA9te:8cFhbwxhl0vyO5Hg@geo.iproyal.com:12321",
+    "http://PpCMxtvpv1VpA9te:8cFhbwxhl0vyO5Hg@geo.iproyal.com:12321",
+    "http://PpCMxtvpv1VpA9te:8cFhbwxhl0vyO5Hg@geo.iproyal.com:12321",
+    "http://PpCMxtvpv1VpA9te:8cFhbwxhl0vyO5Hg@geo.iproyal.com:12321",
+    "http://PpCMxtvpv1VpA9te:8cFhbwxhl0vyO5Hg@geo.iproyal.com:12321",
+    "http://PpCMxtvpv1VpA9te:8cFhbwxhl0vyO5Hg@geo.iproyal.com:12321",
+    "http://PpCMxtvpv1VpA9te:8cFhbwxhl0vyO5Hg@geo.iproyal.com:12321",
+    "http://PpCMxtvpv1VpA9te:8cFhbwxhl0vyO5Hg@geo.iproyal.com:12321",
+    "http://PpCMxtvpv1VpA9te:8cFhbwxhl0vyO5Hg@geo.iproyal.com:12321",
+    "http://PpCMxtvpv1VpA9te:8cFhbwxhl0vyO5Hg@geo.iproyal.com:12321"
 ]
 
-# 🔁 Create an infinite cycle of proxies
 proxy_cycle = itertools.cycle(proxies)
 
-# ✅ Get the next proxy in the list (rotates forever)
 def get_next_proxy():
     return next(proxy_cycle)
